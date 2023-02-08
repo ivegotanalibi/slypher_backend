50d2ce05c4f70fc9982145f9c5c33ed410c34c50
ANIFOWOSE DAMILOLA <damigafardami@gmail.com>
2023-02-08 18:57:52 +0100

    first commit

---
+++ ToDoProj/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿//using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+//using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoProj.Models;
+
+namespace ToDoProj
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //setting the composite primary key of the relationship entity, using fluent api
+            /*modelBuilder.Entity<MoviesActors>()
+                .HasKey(x => new { x.ActorId, x.MovieId });
+
+            modelBuilder.Entity<MoviesGenres>()
+                .HasKey(x => new { x.GenreId, x.MovieId });
+
+            modelBuilder.Entity<MovieTheatersMovies>()
+                .HasKey(x => new { x.MovieTheaterId, x.MovieId });*/
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ToDo> Todos { get; set; }
+
+
+        /*public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<MovieTheater> MovieTheaters { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+        public DbSet<MovieTheatersMovies> MovieTheatersMovies { get; set; }*/
+    }
+}
---
+++ ToDoProj/Controllers/ToDoController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoProj.Migrations;
+using ToDoProj.Models;
+
+namespace ToDoProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoController(ApplicationDbContext context)
+        {
+            _context = context;
+
+            if (_context.Todos.Count() == 0)
+            {
+                // Create a new Todo if collection is empty,
+                // which means you can't delete all Todos.
+                _context.Todos.Add(new ToDo { Title = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todos
+        [HttpGet]
+        public ActionResult<List<ToDo>> GetAll()
+        {
+            return _context.Todos.ToList();
+        }
+
+        // GET: api/Todos/5
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<ToDo> GetById(int id)
+        {
+            var todo = _context.Todos.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return todo;
+        }
+
+        // POST: api/Todos
+        [HttpPost]
+        public ActionResult<ToDo> Create(ToDo todo)
+        {
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = todo.Id }, todo);
+        }
+
+        // PUT: api/Todos/5
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, ToDo todo)
+        {
+            var existingTodo = _context.Todos.Find(id);
+            if (existingTodo == null)
+            {
+                return NotFound();
+            }
+
+            existingTodo.Title = todo.Title;
+            existingTodo.IsComplete = todo.IsComplete;
+
+            _context.Todos.Update(existingTodo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        // DELETE: api/Todos/5
+        [HttpDelete("{id}")]
+        public ActionResult<ToDo> Delete(int id)
+        {
+            var existingTodo = _context.Todos.Find(id);
+            if (existingTodo == null)
+            {
+                return NotFound();
+            }
+             
+            _context.Todos.Remove(existingTodo);
+            _context.SaveChanges();
+            return NoContent();
+        }  
+    }
+}
